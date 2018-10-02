3b914b66cbd5e7269caa01750d8ad3db96fe92e9
odeladetunji <odeladetunji@gmail.com>
2018-10-02 08:55:07 +0100

    committing

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -37,10 +37,59 @@
 
         		<h1>Build Systems Inc.</h1>
         		<h1>Automate Systems,</h1>
-        		<p>Design, Create, impliment and Automate any industrial or business process. Whatever task your organisation or company do everyday in a repetitive manner, we will automate it with algorithms to foster greater productivity in your endevour</p>
+        		<p>Design, Create, impliment and Automate any industrial or business process. Whatever task your organisation or company do everyday in a repetitive manner, we will automate it with algorithms to foster greater productivity in your endeavours</p>
         		<button>Contact</button>
         	</div>
+        	<div class="howWeAutomateProcess">
+        		<h2>How We Automate Process,</h2>
+        	</div>
+        	<div class="row">
+        		<div class="col-4-sm info">
+        			<div>
+        				<i class="icom"></i>
+        				<p class="Design">Design</p>
+        				<p class="description">description</p>
+        			</div>
+        		</div>
+        		<div class="col-4-sm info">
+        			<div>
+        				<i class="icom"></i>
+        				<p class="Design">Design</p>
+        				<p class="description">description</p>
+        			</div>
+        		</div>
+        		<div class="col-4-sm info">
+        			<div>
+        				<i class="icom"></i>
+        				<p class="Design">Design</p>
+        				<p class="description">description</p>
+        			</div>
+        		</div>
+        	</div>
         	<div class="partners">
+        		
+        	</div>
+        	<div class="row">
+        		<h2>Directors</h2>
+        	</div>
+        	<div class="row ourTeam">
+        		<div class="col-4-sm team">
+        			<div id="firstPicture"></div>
+        			<p class="rank">Chief Strategist/Project Manager</p>
+        			<button class="viewProfile">view profile</button>
+        		</div>
+        		<div class="col-4-sm team">
+        			<div id="secondPicture"></div>
+        			<p class="rank">Software Architect (Lead Engineer)</p>
+        			<button class="viewProfile">view profile</button>
+        		</div>
+        		<div class="col-4-sm team">
+        			<div id="thirdPicture"></div>
+        			<p class="rank">Brand Manager/Content Developer</p>
+        			<button class="viewProfile">view profile</button>
+        		</div>
+        	</div>
+        	<div class="aboutUs">
         		
         	</div>
         	<div class="row footer">
