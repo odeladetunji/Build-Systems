d7eeabffbd6c896ab5488a2a98ceb8639bd638c3
odeladetunji <odeladetunji@gmail.com>
2018-10-02 18:53:45 +0100

    committing:

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -3,97 +3,154 @@
 <head>
 	 <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Build-Systems</title>
         <base href="http://127.0.0.1:8000/">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link href="/CSS/landingpage.css" rel="stylesheet" type="text/css">
-        <link href="/CSS/admin.css" rel="stylesheet" type="text/css">
+        <link href="/CSS/build.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <script src="/bootstrap/js/bootstrap.min.js" type="text/JavaScript"></script>
         <!-- JQuery -->
         <script src="/js/jquery-3.2.1.min(first).js" type='text/JavaScript'></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
         <!-- Styles -->
 </head>
 <body>
         <div>
         	<div class="row header">
-        		<ul>
-        			<li>Build System</li>
-        			<li>Products</li>
-        			<li>Announcement</li>
-        			<li>Annoucements</li>
-        			<li>Contact</li>
-        		</ul>
+        		<div class="col-4-sm">
+        			<i></i>
+        		</div>
+        		<div class="col-4-sm">
+        			<ul>
+	        			<li>Build System</li>
+	        			<li>Products</li>
+	        			<li>Announcement</li>
+	        			<li>Contact</li>
+        		    </ul>
+        		</div>
         	</div>
         	<div class="row headBanner">
-        		<ul>
-        			<li>Build System</li>
-        			<li>Products</li>
-        			<li>Annoucements</li>
-        			<li>Contact</li>
-        		</ul>
+        		<div class="col-12-sm subBanner">
+		        		<ul id="subHeadNavigation">
+		        			<li id="buildlist">Contact</li>
+		        			<li>Announcement</li>
+		        			<li>Products</li>
+		        			<li>Build System</li>
+		        		</ul>
 
-        		<h1>Build Systems Inc.</h1>
-        		<h1>Automate Systems,</h1>
-        		<p>Design, Create, impliment and Automate any industrial or business process. Whatever task your organisation or company do everyday in a repetitive manner, we will automate it with algorithms to foster greater productivity in your endeavours</p>
-        		<button>Contact</button>
+		        		<h1 id="firstTitle">Build Systems Inc.</h1>
+		        		<h1>Automate Systems,</h1>
+		        		<p>Design, Automate and Impliment any industrial or <br> business process. Whatever task your organisation <br> do everyday in a repetitive manner, we will automate it with <br> algorithms to foster greater productivity in your endeavours</p>
+		        		<button>Contact</button>
+        		</div>
         	</div>
         	<div class="howWeAutomateProcess">
-        		<h2>How We Automate Process,</h2>
+        		<h2>How We Automate Processes??</h2>
         	</div>
-        	<div class="row">
-        		<div class="col-4-sm info">
-        			<div>
-        				<i class="icom"></i>
-        				<p class="Design">Design</p>
-        				<p class="description">description</p>
+        	<div class="row theHow">
+        		<div class="col-sm-4">
+        			<div class="content">
+        				<i class="fas fa-pen icon"></i>
+        				<p class="design">Design Systems</p>
+        				<p class="description">This first stage is were we work in sync with clients to develop detailed documents of the activities our clients do in their organisations. we study and come up with a flow diagram of the activities.</p>
         			</div>
         		</div>
-        		<div class="col-4-sm info">
-        			<div>
-        				<i class="icom"></i>
-        				<p class="Design">Design</p>
-        				<p class="description">description</p>
+        		<div class="col-sm-4">
+        			<div class="content">
+        				<i class="fa fa-cog icon"></i>
+        				<p class="design">Automate Proceesses</p>
+        				<p class="description">The design stage is followed by choosing technology stacks/framework and using the technologies to write algorithms for automation of the processes detailed in the design stage.
+</p>
         			</div>
         		</div>
-        		<div class="col-4-sm info">
-        			<div>
-        				<i class="icom"></i>
-        				<p class="Design">Design</p>
-        				<p class="description">description</p>
+        		<div class="col-sm-4">
+        			<div class="content">
+        				<i class="fa fa-cloud icon"></i>
+        				<p class="design">Implimentation</p>
+        				<p class="description">The final stage, this is were the finished products are tested and deployed to the nessary environments or machines for consumption.</p>
         			</div>
         		</div>
         	</div>
-        	<div class="partners">
+        	<div class="row partners">
         		
         	</div>
-        	<div class="row">
+        	<div class="row directors">
         		<h2>Directors</h2>
         	</div>
         	<div class="row ourTeam">
-        		<div class="col-4-sm team">
+        		<div class="col-sm-4 team">
         			<div id="firstPicture"></div>
+        			<p class="name">Segun Offiong</p>
         			<p class="rank">Chief Strategist/Project Manager</p>
         			<button class="viewProfile">view profile</button>
         		</div>
-        		<div class="col-4-sm team">
+        		<div class="col-sm-4 team">
         			<div id="secondPicture"></div>
+        			<p class="name">Odelade Olatunji</p>
         			<p class="rank">Software Architect (Lead Engineer)</p>
         			<button class="viewProfile">view profile</button>
         		</div>
-        		<div class="col-4-sm team">
+        		<div class="col-sm-4 team">
         			<div id="thirdPicture"></div>
+        			<p class="name">Ayo</p>
         			<p class="rank">Brand Manager/Content Developer</p>
         			<button class="viewProfile">view profile</button>
         		</div>
         	</div>
-        	<div class="aboutUs">
-        		
+        	<div class="row aboutUs">
+        		<h3>Who We are</h3>
+        		<p>we ara bla bla bla ....</p>
+
+        		<h3>Our Technologies</h3><br>
+        		<h5>Languages Stack</h5>
+        		<button>Java</button>
+        		<button>Python</button>
+        		<button>Objective C</button>
+        		<button>Swift</button>
+        		<button>C#</button>
+        		<button>C</button>
+        		<button>JavaScript</button>
+        		<button>React</button>
+        		<button>Spring Boot</button>
+        		<button>Spring MVC</button>
+        		<button>ASP.net MVC</button>
+        		<button>Android</button>
+        		<button>Electron</button>
+        		<button>Php Laravel</button>
+        		<button>Aws</button>
+        		<button>Linux</button><br><br>
+        		<h5>Design Tools</h5>
+        		<button>Adobe Illustrator</button>
+        		<button>Adobe Photoshop</button>
+        		<button>Principle</button>
+        		<button>Sketch</button>
+
+        		<h3 id="lastH3">Mission</h3><br><br>
+        		<p>to make life easier for people....</p><br>
+        		<h3>Our Partners</h3>
+        		<div class="imagesDiv">
+        			<img src="/storage/images/google.png" alt="" class="images">
+        			<img src="/storage/images/aws.png" alt="" class="images">
+        			<img src="/storage/images/digitaloceans.png" alt="" class="images">
+        			<img src="/storage/images/namecheap.png" alt="">
+        		</div>
         	</div>
         	<div class="row footer">
-        		
+        		<form action="">
+        			<p>send us a message!</p>
+        			<input type="email" placeholder="email">
+        			<textarea name="" id="" cols="30" rows="10" placeholder="please enter your quest below and we will get back to you"></textarea>
+        			<button>submit!</button>
+        		</form>
+        		<p>Copyright @buildsystems inc 2018</p>
         	</div>
         </div>
 </body>
