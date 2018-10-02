b030a8956f354099769b630839392e7168e38900
odeladetunji <odeladetunji@gmail.com>
2018-10-02 08:08:42 +0100

    committing:

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Build-Systems</title>
+        <base href="http://127.0.0.1:8000/">
+
+        <link href="/CSS/landingpage.css" rel="stylesheet" type="text/css">
+        <link href="/CSS/admin.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <script src="/bootstrap/js/bootstrap.min.js" type="text/JavaScript"></script>
+        <!-- JQuery -->
+        <script src="/js/jquery-3.2.1.min(first).js" type='text/JavaScript'></script>
+        <!-- Styles -->
+</head>
+<body>
+        <div>
+        	<div class="row header">
+        		<ul>
+        			<li>Build System</li>
+        			<li>Products</li>
+        			<li>Announcement</li>
+        			<li>Annoucements</li>
+        			<li>Contact</li>
+        		</ul>
+        	</div>
+        	<div class="row headBanner">
+        		<ul>
+        			<li>Build System</li>
+        			<li>Products</li>
+        			<li>Annoucements</li>
+        			<li>Contact</li>
+        		</ul>
+
+        		<h1>Build Systems Inc.</h1>
+        		<h1>Automate Systems,</h1>
+        		<p>Design, Create, impliment and Automate any industrial or business process. Whatever task your organisation or company do everyday in a repetitive manner, we will automate it with algorithms to foster greater productivity in your endevour</p>
+        		<button>Contact</button>
+        	</div>
+        	<div class="partners">
+        		
+        	</div>
+        	<div class="row footer">
+        		
+        	</div>
+        </div>
+</body>
+</html>
